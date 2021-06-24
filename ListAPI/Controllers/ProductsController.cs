2bc4d6da53d595924de34f99686f1690af4c5b89
Deepak Mehta (EXT/DIGITAL) <deepak.mehta@petronas.com>
2021-06-24 14:01:54 +0800

    initial commit

---
+++ ListAPI/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ListAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Listing> Get()
+        {
+            var list = new List<Listing>
+            {
+                new Listing
+                {
+                    Id=1,
+                    Description="A very old boat. Bargain price",
+                    Name="Old Boat",
+                    Price=3000
+                },
+                new Listing
+                {
+                    Id=2,
+                    Description="Computer",
+                    Name="From the 1990s, a classic!",
+                    Price=4000
+                },
+                new Listing
+                {
+                    Id=3,
+                    Description="Good condition, free delivery",
+                    Name="Basketball Hoop",
+                    Price=3000
+                },
+                 new Listing
+                {
+                    Id=4,
+                    Description="Bicycle",
+                    Name="Bright blue, very fast",
+                    Price=1000
+                },
+            };
+
+            return list;
+        }
+
+        [HttpGet("myListings")]
+        public IEnumerable<Listing> myListings()
+        {
+            var list = new List<Listing>
+            {
+                new Listing
+                {
+                    Id=1,
+                    Description="A very old boat. Bargain price",
+                    Name="Old Boat",
+                    Price=3000
+                },
+                new Listing
+                {
+                    Id=2,
+                    Description="Computer",
+                    Name="From the 1990s, a classic!",
+                    Price=4000
+                }
+            };
+
+            return list;
+        }
+    }
+
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ListAPI/Extensions/AuthenticationExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAPI.Extensions
+{
+    public static class AuthenticationExtension
+    {
+        public static IServiceCollection AddTokenAuthentication(this IServiceCollection services, IConfiguration config)
+        {
+            var secret = config.GetSection("AzureAd").GetSection("secret").Value;
+
+            var key = Encoding.ASCII.GetBytes(secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = "https://sts.windows.net/8b24551d-7c2c-4beb-8b61-95f32d9929ef/",
+                    ValidAudience = "6365b475-62ee-48f1-b746-351c419efff4"
+                };
+            });
+
+            return services;
+        }
+    }
+}
